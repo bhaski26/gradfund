b8e8959f35f63e4c3e6555ebcad3eb77adafc63d
Bhaskar A Gowda <bhaskargowda726@gmail.com>
2026-07-04 11:52:45 +0530

    Fix bcrypt compatibility issue

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.schemas.user import UserCreate
+from app.models.user import User
+from app.database.dependencies import get_db
+from app.utils.security import hash_password
 
 router = APIRouter(
     prefix="/auth",
@@ -7,7 +13,39 @@ router = APIRouter(
 
 
 @router.post("/register")
-def register():
+def register(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    existing_user = (
+        db.query(User)
+        .filter(User.email == user.email)
+        .first()
+    )
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    hashed_password = hash_password(
+        user.password
+    )
+
+    new_user = User(
+        full_name=user.full_name,
+        email=user.email,
+        hashed_password=hashed_password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
     return {
-        "message": "Register endpoint working"
+        "id": new_user.id,
+        "full_name": new_user.full_name,
+        "email": new_user.email,
+        "message": "User registered successfully"
     }
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,20 +1,33 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime
+)
 from sqlalchemy.sql import func
+
 from app.database.session import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
 
-    full_name = Column(String(100), nullable=False)
+    full_name = Column(
+        String(100),
+        nullable=False
+    )
 
     email = Column(
         String(255),
         unique=True,
-        index=True,
-        nullable=False
+        nullable=False,
+        index=True
     )
 
     hashed_password = Column(
